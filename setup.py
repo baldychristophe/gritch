6ee64c89e9cf2cc6f87fff0cbc03ad97c052f10a
Christophe Baldy <baldychristophe@gmail.com>
2023-12-01 17:39:54 +0100

    Add missing dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 
 setup(
     install_requires=[
+        'beautifulsoup4',
         'click',
         'diskcache',
         'Pillow',
         'PyGithub',
         'requests',
         'rich',
+        'rich_pixel',
         'textual',
     ],
 )
